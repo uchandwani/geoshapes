543831f1616dbaa4c4221759d6f1a249007881f2
Umesh Chandwani <chandwani.umesh@gmail.com>
2025-04-10 11:37:28 +0530

    reference template

--- dev/js/header.js
+++ dev/js/header.js
@@ -9,6 +9,7 @@ const pageTitles = {
   "triangle_theorem_07.html": "Triangle Theorems",
   "trig_properties_09.html": "Trigonometric Properties",
   "circle_theorems_02.html": "Circle Theorems"
+   "PLT.html": "Referenct Theorems"
 };
 console.log("PageTitle is now declared");
 function updatePageTitle() {
