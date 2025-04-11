32043d8cae031b55e37b6f7b2d13df4b2a65bba6
Umesh Chandwani <chandwani.umesh@gmail.com>
2025-04-11 11:00:45 +0530

    theorem definition

--- dev/js/parallelLinesPage.js
+++ dev/js/parallelLinesPage.js
@@ -15,7 +15,7 @@
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
         canvas.width = 700;
-        canvas.height = 700;
+        canvas.height = 800;
 
         
       document.querySelectorAll('svg[id$="-button"]').forEach(svgBtn => {
