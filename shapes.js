01a9bb15dca558274241bb6cae39c93eda6a643c
Umesh Chandwani <chandwani.umesh@gmail.com>
2025-04-25 12:31:35 +0530

    removed lines in mousedown and mouseup to  avoid canvas block

--- shapes.js
+++ shapes.js
@@ -272,7 +272,7 @@ export function generatePointLabel() {
 canvas.addEventListener('mousedown', (e) => {
     const { offsetX, offsetY } = e;
 
-    document.getElementById('right-sidebar').style.pointerEvents = 'none';
+    
 
     if (mode === 'modify') {
         // Detect shape under the cursor
@@ -559,8 +559,7 @@ canvas.addEventListener('mousemove', (e) => {
 canvas.addEventListener('mouseup', (e) => {
     console.log(`Inside mouse up Mouse Up at: X = ${e.offsetX}, Y = ${e.offsetY}`);
     
-    document.getElementById('right-sidebar').style.pointerEvents = 'auto';
-
+    
     if (!selectedShape) {
         console.log("No shape was selected before mouse up. Exiting safely.");
         return; // 🔥 Avoid accessing undefined properties
