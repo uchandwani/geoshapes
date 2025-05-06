0ccf7bf2282ddcec71932e13b45281ecd7c6f632
Umesh Chandwani <chandwani.umesh@gmail.com>
2025-05-06 11:37:41 +0530

    SVG

--- js/trianglePropConfig.js
+++ js/trianglePropConfig.js
@@ -96,10 +96,13 @@ export  const functionalityConfig = {
                     type: "cos",
                     svg: `
                         <svg class="sub-button-svg" width="36" height="36" viewBox="0 0 100 100">
-                        <circle cx="50" cy="50" r="48" stroke="black" fill="none" stroke-width="2" />
-                        <line x1="10" y1="90" x2="10" y2="10" stroke="red" stroke-width="2"/>
-                        <line x1="10" y1="10" x2="90" y2="10" stroke="red" stroke-width="2"/>
-                        <line x1="10" y1="90" x2="90" y2="10" stroke="blue" stroke-width="2"/>
+                        <circle cx="0" cy="100" r="100" stroke="black" stroke-width="2" fill="none" />
+                        <line x1="0" y1="100" x2="100" y2="100" stroke="red" stroke-width="2" />
+                        <line x1="0" y1="100" x2="0" y2="0" stroke="red" stroke-width="2" />
+                        <line x1="0" y1="100" x2="80" y2="20" stroke="blue" stroke-width="2" />
+                        <line x1="20" y1="100" x2="20" y2="40" stroke="blue" stroke-width="2" />
+                        <line x1="80" y1="20" x2="80" y2="100" stroke="blue" stroke-width="2" />
+                        <line x1="20" y1="40" x2="80" y2="100" stroke="blue" stroke-width="2" />
                     </svg>
                             `
                         },
@@ -327,10 +330,42 @@ export  const functionalityConfig = {
             ]
           },
           
-        buttonSet: [
-                { label: "regular", type: "sin" },
-                { label: "edge", type: "cos" },
-                
+          buttonSet: [
+
+            {
+                label: "Regular",
+                type: "sin",
+                svg: `
+                 <svg class="sub-button-svg" width="36" height="36" viewBox="0 0 100 100">
+                   <circle cx="0" cy="100" r="100" stroke="black" stroke-width="2" fill="none" />
+                    <line x1="0" y1="100" x2="100" y2="100" stroke="black" stroke-width="2" />
+                    <line x1="0" y1="0" x2="0" y2="100" stroke="black" stroke-width="2" />
+                    <line x1="0" y1="100" x2="80" y2="20" stroke="blue" stroke-width="2" />
+                    <line x1="0" y1="100" x2="60" y2="40" stroke="blue" stroke-width="2" />
+                    <line x1="0" y1="100" x2="40" y2="60" stroke="blue" stroke-width="2" />
+                    <line x1="40" y1="60" x2="40" y2="100" stroke="blue" stroke-width="2" />
+                    <line x1="60" y1="40" x2="60" y2="100" stroke="blue" stroke-width="2" />
+                    <line x1="80" y1="20" x2="80" y2="100" stroke="blue" stroke-width="2" />
+                </svg>
+                                        `
+                  },
+                  {
+                label: "Edge",
+                type: "cos",
+                svg: `
+                    <svg class="sub-button-svg" width="36" height="36" viewBox="0 0 100 100">
+                    <circle cx="0" cy="100" r="100" stroke="black" stroke-width="2" fill="none" />
+                    <line x1="0" y1="100" x2="100" y2="100" stroke="red" stroke-width="2" />
+                    <line x1="0" y1="100" x2="0" y2="0" stroke="red" stroke-width="2" />
+                    <line x1="0" y1="100" x2="80" y2="20" stroke="blue" stroke-width="2" />
+                    <line x1="20" y1="100" x2="20" y2="40" stroke="blue" stroke-width="2" />
+                    <line x1="80" y1="20" x2="80" y2="100" stroke="blue" stroke-width="2" />
+                    <line x1="20" y1="40" x2="80" y2="100" stroke="blue" stroke-width="2" />
+                </svg>
+                        `
+                    },
+                 
+                               
             ],
         leftSidebarContent: `
 
