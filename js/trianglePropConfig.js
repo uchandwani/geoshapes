6bf583989f7bff278aff602a6e14d15c90ba3b58
Umesh Chandwani <chandwani.umesh@gmail.com>
2025-05-06 11:32:09 +0530

    SVG

--- js/trianglePropConfig.js
+++ js/trianglePropConfig.js
@@ -79,11 +79,15 @@ export  const functionalityConfig = {
                     type: "sin",
                     svg: `
                      <svg class="sub-button-svg" width="36" height="36" viewBox="0 0 100 100">
-                        <circle cx="50" cy="50" r="48" stroke="black" fill="none" stroke-width="2" />
-                        <line x1="10" y1="90" x2="90" y2="90" stroke="blue" stroke-width="2"/>
-                        <line x1="10" y1="90" x2="30" y2="20" stroke="blue" stroke-width="2"/>
-                        <line x1="30" y1="20" x2="70" y2="40" stroke="blue" stroke-width="2"/>
-                        <line x1="70" y1="40" x2="90" y2="90" stroke="blue" stroke-width="2"/>
+                       <circle cx="0" cy="100" r="100" stroke="black" stroke-width="2" fill="none" />
+                        <line x1="0" y1="100" x2="100" y2="100" stroke="black" stroke-width="2" />
+                        <line x1="0" y1="0" x2="0" y2="100" stroke="black" stroke-width="2" />
+                        <line x1="0" y1="100" x2="80" y2="20" stroke="blue" stroke-width="2" />
+                        <line x1="0" y1="100" x2="60" y2="40" stroke="blue" stroke-width="2" />
+                        <line x1="0" y1="100" x2="40" y2="60" stroke="blue" stroke-width="2" />
+                        <line x1="40" y1="60" x2="40" y2="100" stroke="blue" stroke-width="2" />
+                        <line x1="60" y1="40" x2="60" y2="100" stroke="blue" stroke-width="2" />
+                        <line x1="80" y1="20" x2="80" y2="100" stroke="blue" stroke-width="2" />
                     </svg>
                                             `
                       },
